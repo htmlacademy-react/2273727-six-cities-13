0c292b849947e685d82274c21978db489a7f5118
EgorPestov <egorpestov2012@yandex.ru>
2023-07-11 18:27:24 +0400

    feat: сделана маршрутизация страниц

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,44 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainPage from '../../pages/main/main';
+import Favorites from '../../pages/favorites/favorites';
+// import FavoritesEmpty from '../../pages/favorites/favorites-empty';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+// import OfferNotLogged from '../../pages/offer/offer-not-logged';
+import { NotFound } from '../../pages/404/404';
+
 
 type AppProps = {
   cardsCount: number;
 }
 
-function App({cardsCount}: AppProps) : JSX.Element {
+function App({ cardsCount }: AppProps): JSX.Element {
   return (
-    <MainPage cardsCount={cardsCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage cardsCount={cardsCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,10 @@
 export const Settings = {
   CardsCount: 5,
 } as const;
+
+export enum AppRoute {
+  Root = '/',
+  Favorites = '/favorites',
+  Login = '/login',
+  Offer = '/offer',
+}
