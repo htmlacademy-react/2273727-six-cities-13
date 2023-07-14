71a9eed7bd555e7a234cb7f37f38d3385eeaef7e
EgorPestov <egorpestov2012@yandex.ru>
2023-07-14 11:07:39 +0400

    feat: добавил использование NavLink для шапки

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AppRoute } from '../../const';
+
+
+const getStyleForNavLink = ({ isActive }: { isActive: boolean }) =>
+  isActive
+    ? {
+      cursor: 'default',
+    }
+    : {};
 
 export function Header() {
   return (
@@ -7,7 +15,7 @@ export function Header() {
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <Link className="header__logo-link" to={AppRoute.Root}>
+            <NavLink className="header__logo-link" to={AppRoute.Root} style={getStyleForNavLink}>
               <img
                 className="header__logo"
                 src="img/logo.svg"
@@ -15,21 +23,22 @@ export function Header() {
                 width={81}
                 height={41}
               />
-            </Link>
+            </NavLink>
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <Link
+                <NavLink
                   className="header__nav-link header__nav-link--profile"
                   to={AppRoute.Favorites}
+                  style={getStyleForNavLink}
                 >
                   <div className="header__avatar-wrapper user__avatar-wrapper"></div>
                   <span className="header__user-name user__name">
                     Oliver.conner@gmail.com
                   </span>
                   <span className="header__favorite-count">3</span>
-                </Link>
+                </NavLink>
               </li>
               <li className="header__nav-item">
                 <Link className="header__nav-link" to={AppRoute.Login}>
