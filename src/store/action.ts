4ecc997a073b7a25fca3be9e0a796fd15a4f6407
EgorPestov <egorpestov2012@yandex.ru>
2023-08-03 16:54:45 +0400

    feat: все подготовлено для приема данных с сервера

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,6 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 import { OfferType } from '../components/types/offer';
 import { FullOfferType } from '../components/types/full-offer';
+import { AuthorizationStatus } from '../const';
 
 export const setActiveCity = createAction('setActiveCity', (city: string | undefined) => ({ payload: city }));
 
@@ -9,3 +10,7 @@ export const setSortType = createAction('setSortType', (sortType: string) => ({ 
 export const setOffers = createAction('getOffers', (offers: OfferType[]) => ({ payload: offers }));
 
 export const setFullOffers = createAction('getFullOffers', (fullOffers: FullOfferType[]) => ({ payload: fullOffers }));
+
+export const loadOffers = createAction('loadOffers', (offers: OfferType[]) => ({ payload: offers }));
+
+export const requireAuthorization = createAction('requireAuthorization', (authorizationStatus: AuthorizationStatus) => ({ payload: authorizationStatus }));
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,15 +1,17 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { setActiveCity, setSortType, setOffers, setFullOffers } from './action';
+import { setActiveCity, setSortType, setOffers, setFullOffers, loadOffers, requireAuthorization } from './action';
 import { OfferType } from '../components/types/offer';
 import { FullOfferType } from '../components/types/full-offer';
 import { offers } from '../mocks/offers';
 import { fullOffers } from '../mocks/full-offers';
+import { AuthorizationStatus } from '../const';
 
 export type InitialStateType = {
   activeCity: string | undefined;
   offers: OfferType[];
   fullOffers: FullOfferType[];
   activeSortType: string;
+  authorizationStatus: AuthorizationStatus;
 }
 
 const initialState: InitialStateType = {
@@ -17,6 +19,7 @@ const initialState: InitialStateType = {
   offers: offers,
   fullOffers: fullOffers,
   activeSortType: 'Popular',
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
@@ -32,5 +35,11 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(setFullOffers, (state, action) => {
       state.fullOffers = action.payload;
+    })
+    .addCase(loadOffers, (state, action) => {
+      state.offers = action.payload;
+    })
+    .addCase(requireAuthorization, (state, action) => {
+      state.authorizationStatus = action.payload;
     });
 });
