a279be779a1235504396229ecddbd6a1549de343
EgorPestov <egorpestov2012@yandex.ru>
2023-08-13 14:17:05 +0400

    feat: емейл берется из юзердаты при логине

--- src/store/user-process.ts/selectors.ts
+++ src/store/user-process.ts/selectors.ts
@@ -1,6 +1,6 @@
-import { NameSpace, AuthStatusValuesType, AuthStatus } from '../../const';
+import { NameSpace, AuthStatusValuesType } from '../../const';
 import { State } from '../../hooks/useAppSelector/useAppSelector';
 
 export const getAuthStatus = (state: State): AuthStatusValuesType => state[NameSpace.User].authorizationStatus;
-export const getAuthCheckedStatus = (state: State): boolean => state[NameSpace.User].authorizationStatus !== AuthStatus.Unknown;
+export const getEmail = (state: State): string | undefined => state[NameSpace.User].userData?.email;
 
