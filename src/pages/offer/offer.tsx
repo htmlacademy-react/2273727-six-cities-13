8a72a5cf600dde90d99deb58b107e125f215f56e
EgorPestov <egorpestov2012@yandex.ru>
2023-08-14 19:46:11 +0400

    feat: компонент offer разгружен от излишних вычислений

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -13,10 +13,9 @@ import { useAppDispatch } from '../../hooks/useAppDispatch/useAppDispatch';
 import { fetchNearbyOffers, fetchFullOffer } from '../../store/api-actions';
 import { LoadingScreen } from '../loading-screen/loading-screen';
 import { NotFound } from '../404/404';
-import { setActiveId } from '../../store/offers-process/offers-process';
+import { setActiveId, setCurrentOffer } from '../../store/offers-process/offers-process';
 import { RATING_COEFFICIENT } from '../../const';
-import { OfferType } from '../../components/types/offer';
-import { getOffers, getOffersLoadStatus, getFullOffer, getFullOfferLoadStatus } from '../../store/offers-process/selectors';
+import { getOffers, getCurrentOffer, getOffersLoadStatus, getFullOffer, getFullOfferLoadStatus } from '../../store/offers-process/selectors';
 import { getNearbyOffers, getNearbyOffersLoadStatus } from '../../store/nearby-offers-process/selectors';
 
 export const Offer = () => {
@@ -33,12 +32,13 @@ export const Offer = () => {
     dispatch(fetchFullOffer({ id: offerId }));
     dispatch(fetchNearbyOffers({ id: offerId }));
     dispatch(setActiveId(offerId));
+    dispatch(setCurrentOffer());
   }, [isIdExist, offerId, dispatch]
   );
 
   const offer = useAppSelector(getFullOffer);
   const loadedNearbyOffers = useAppSelector(getNearbyOffers);
-
+  const currentOffer = useAppSelector(getCurrentOffer);
 
   const isOfferLoading = useAppSelector(getFullOfferLoadStatus);
   const isNearbyOfferLoading = useAppSelector(getNearbyOffersLoadStatus);
@@ -52,11 +52,14 @@ export const Offer = () => {
     );
   }
 
-  if (isPageLoading || isSomethingMissingFromServer) {
+  if (isPageLoading || isSomethingMissingFromServer || currentOffer === null) {
     return (
       <LoadingScreen />
     );
   }
+
+  const currentCity = loadedNearbyOffers[0].city;
+  const nearbyOffers = [...loadedNearbyOffers, currentOffer];
 
   const { bedrooms, city, description, goods, id, host, images, isFavorite, isPremium, maxAdults, price, rating, title, type } = offer;
 
@@ -65,10 +68,6 @@ export const Offer = () => {
       return { fill: '#4481c3', stroke: '#4481c3' };
     }
   };
-
-  const currentCity = loadedNearbyOffers[0].city;
-  const currentOffer = offers.find((item) => item.id === offerId) as OfferType;
-  const nearbyOffers = [...loadedNearbyOffers, currentOffer];
 
   return (
     <div className="page">
