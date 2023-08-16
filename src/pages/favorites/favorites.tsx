a8a01f6599c00c5cad01ebc50912ccd8647c1aaa
EgorPestov <egorpestov2012@yandex.ru>
2023-08-16 20:39:07 +0400

    feat: добавлена заглушка к favorites

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -6,8 +6,10 @@ import { useAppDispatch } from '../../hooks/useAppDispatch/useAppDispatch';
 import { Header } from '../../components/header/header';
 import { AppRoute } from '../../const';
 import { FavoritesList } from '../../components/favorites-list/favorites-list';
-import { getFavOffers } from '../../store/offers-process/selectors';
+import { getFavOffers, getFavOffersLoadStatus } from '../../store/offers-process/selectors';
 import { fetchFavOffers } from '../../store/api-actions';
+import { FavoritesEmpty } from './favorites-empty';
+import { LoadingScreen } from '../loading-screen/loading-screen';
 
 export function Favorites() {
   const dispatch = useAppDispatch();
@@ -15,6 +17,11 @@ export function Favorites() {
     dispatch(fetchFavOffers());
   }, [dispatch]);
   const favorites = useAppSelector(getFavOffers);
+  const isFavOffersLoading = useAppSelector(getFavOffersLoadStatus);
+
+  if (isFavOffersLoading) {
+    return <LoadingScreen />;
+  }
 
   return (
     <div className="page">
@@ -24,16 +31,19 @@ export function Favorites() {
 
       <Header />
 
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              <FavoritesList favorites={favorites} />
-            </ul>
-          </section>
-        </div>
-      </main>
+      {favorites.length === 0 && !isFavOffersLoading ?
+        <FavoritesEmpty /> :
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                <FavoritesList favorites={favorites} />
+              </ul>
+            </section>
+          </div>
+        </main>}
+
       <footer className="footer container">
         <Link className="footer__logo-link" to={AppRoute.Root}>
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
