e77991734beab72d25576a053e28f129f68cf346
EgorPestov <egorpestov2012@yandex.ru>
2023-08-16 17:24:48 +0400

    feat: обновлена работа с сортировкой

--- src/components/offer-card/offer-card.tsx
+++ src/components/offer-card/offer-card.tsx
@@ -68,7 +68,7 @@ const OfferCardComponent = (
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className={`place-card__bookmark-button ${isFav ? 'place-card__bookmark-button--active' : ''}  button`} type="button" onClick={onButtonClick}>
+          <button className={`place-card__bookmark-button ${isFavorite ? 'place-card__bookmark-button--active' : ''}  button`} type="button" onClick={onButtonClick}>
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark" />
             </svg>
