524543c131588fef627080225b9a97e3df938902
EgorPestov <egorpestov2012@yandex.ru>
2023-08-25 12:38:21 +0400

    feat: фиксы тестов

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import { MemoryHistory, createMemoryHistory } from 'history';
 import { AppRoute } from '../../const';
 import { App } from './app';
-import { withStore } from '../../store/mocks/mock-component';
+import { withHistory, withStore } from '../../store/mocks/mock-component';
 import { mockStore } from '../../store/mocks/mock-store';
 
 describe('App', () => {
@@ -12,13 +12,35 @@ describe('App', () => {
     mockHistory = createMemoryHistory();
   });
 
-  it('should render "MainScreen" when user navigate to "/"', () => {
-    const { withStoreComponent } = withStore(<App />, mockStore());
+  it('should render "Main" page when user navigate to "/"', () => {
+    const withHistoryComponent = withHistory(<App />, mockHistory);
+    const { withStoreComponent } = withStore(withHistoryComponent, mockStore());
     mockHistory.push(AppRoute.Root);
 
     render(withStoreComponent);
 
-    expect(screen.getByTestId('main-empty')).toBeInTheDocument();
     expect(screen.getByText(/No places to stay available/i)).toBeInTheDocument();
+  });
+
+  it('should render "Login" page when user navigate to "/login"', () => {
+    const withHistoryComponent = withHistory(<App />, mockHistory);
+    const { withStoreComponent } = withStore(withHistoryComponent, mockStore());
+    mockHistory.push(AppRoute.Login);
+
+    render(withStoreComponent);
+
+    expect(screen.getByTestId('login')).toBeInTheDocument();
+  });
+
+  it('should render "NotFound" page when user navigate to non-existent route', () => {
+    const withHistoryComponent = withHistory(<App />, mockHistory);
+    const { withStoreComponent } = withStore(withHistoryComponent, mockStore());
+    const unknownRoute = '/unknown-route';
+    mockHistory.push(unknownRoute);
+
+    render(withStoreComponent);
+
+    expect(screen.getByText('404.')).toBeInTheDocument();
+    expect(screen.getByText('Page not found')).toBeInTheDocument();
   });
 });
